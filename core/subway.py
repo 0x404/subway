16284f519e0ee3aba7ef8cb15e79422191dc459c
1120192092 <1120192092@bit.edu.cn>
2022-01-08 11:28:23 +0800

    bug fixed

--- core/subway.py
+++ core/subway.py
@@ -1,20 +1,18 @@
-from model import SubwaySys
 import data_loader
-import sys
+from model import SubwaySys
+
 
 if __name__ == "__main__":
     lines = data_loader.load_lines("../data/beijing-subway.txt")
     subway = SubwaySys(lines)
     while True:
         cmd = input()
-        cmd = cmd.strip().split(' ')
-        if cmd[0] == '/b' and len(cmd) == 3:
+        cmd = cmd.strip().split(" ")
+        if cmd[0] == "a" and len(cmd) == 3:
             path = subway.shortest_path(cmd[1], cmd[2])
             print(len(path))
             for station in path:
                 if station.is_trans:
-                    print (station.name, " 换乘")
+                    print(station.name, " 换乘")
                 else:
-                    print (station.name)
-
-        
+                    print(station.name)
