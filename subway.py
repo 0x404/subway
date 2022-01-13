f5b5d6497607902752a9e2b7fd16199f8f553f91
1120192092 <1120192092@bit.edu.cn>
2022-01-13 20:52:33 +0800

    refactor subway.py

--- subway.py
+++ subway.py
@@ -1,21 +1,45 @@
 """subway"""
+import sys
 from core import utils
 from core.model import SubwaySys
 
 
-if __name__ == "__main__":
-    # lines from 1 to len(lines - 1)
+def print_path(subway_sys, start, end):
+    """
+    print a path from start to end in subway_sys
+    :param subway_sys: a subwaysys
+    :param start: a string indicates start station's name
+    :param end: a string indicates end station's name
+    """
+    path = subway_sys.shortest_path(start, end)
+    print("总站数: ", len(path))
+    for station, msg in path:
+        print(station.name, msg if msg is not None else "")
+
+
+def main():
+    """
+    subway system entry
+    supported operation:
+        subway.py /b <start_station_name> <end_station_name>
+    """
     lines = utils.load_lines("data/beijing-subway.txt")
     subway = SubwaySys(lines)
-    # subway.test_by_file("../data/testfile.txt")
+    if len(sys.argv) == 4 and sys.argv[1] == "/b":
+        print_path(subway, sys.argv[2], sys.argv[3])
+    elif len(sys.argv) != 1:
+        print("[error]: operation is supported!")
+        print("[usage]: subway.py /b <start_station_name> <end_station_name>")
+        return
     while True:
-        cmd = input()
-        cmd = cmd.strip().split(" ")
-        if cmd[0] == "/b" and len(cmd) == 3:
-            path = subway.shortest_path(cmd[1], cmd[2])
-            print(len(path))
-            for station, msg in path:
-                if msg is not None:
-                    print(station.name, msg)
-                else:
-                    print(station.name)
+        command = input()
+        command = command.strip().split(" ")
+        if command[0] == "/b" and len(command) == 3:
+            print_path(subway, command[1], command[2])
+        else:
+            print("[error]: operation is supported!")
+            print("[usage]: subway.py /b <start_station_name> <end_station_name>")
+
+
+if __name__ == "__main__":
+    main()
