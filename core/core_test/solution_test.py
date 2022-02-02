df595b6c0a0854433c243682dfab9108120a859a
1120192092 <1120192092@bit.edu.cn>
2022-02-02 18:38:41 +0800

    test: add pytest for verify path

--- core/core_test/solution_test.py
+++ core/core_test/solution_test.py
@@ -100,3 +100,26 @@ def test_travel_path_from():
     test1_pred3 = solution.travel_path_from("a_2", test1_data.nexto, test1_data.lines)
     test1_ans3 = ["a_2", "b_1", "c_1", "b_1", "a_1", "b_2", "a_1", "a_2", "a_3", "a_4"]
     assert test1_pred3 == test1_ans3
+
+
+def test_verify_path():
+    test1_line1 = Line(
+        "A",
+        [Station("a_1"), Station("a_2"), Station("a_3"), Station("a_4")],
+        is_ring=True,
+    )
+    test1_line2 = Line("B", [Station("b_1"), Station("a_1"), Station("b_2")])
+    test1_line3 = Line("C", [Station("a_2"), Station("b_1"), Station("c_1")])
+    test1_data = SubwaySys([test1_line1, test1_line2, test1_line3])
+
+    test1_input1 = ["a_1", "a_2", "a_3", "a_4", "b_1", "b_2", "c_1"]
+    test1_pred1 = solution.verify_path(test1_input1, test1_data.nexto)
+    assert test1_pred1["stats"] == "error"
+
+    test1_input2 = ["a_1", "a_2", "a_3", "a_4", "a_1", "b_1", "c_1"]
+    test1_pred2 = solution.verify_path(test1_input2, test1_data.nexto)
+    assert test1_pred2["stats"] == "false" and test1_pred2["miss_st"] == ["b_2"]
+
+    test1_input3 = ["b_2", "a_1", "b_1", "c_1", "b_1", "a_1", "a_4", "a_3", "a_2"]
+    test1_pred3 = solution.verify_path(test1_input3, test1_data.nexto)
+    assert test1_pred3["stats"] == "true"
